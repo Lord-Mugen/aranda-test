17a30fde5b89ed7aceac58e842607a624509b3dd
Christian Ruiz <christianjavieruizcortes@gmail.com>
2022-12-21 11:15:15 -0500

    first commit and settings

---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+
+const Card = () => {
+  return (
+    <>
+      Card
+    </>
+  )
+}
+
+export default Card
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <>
+      Navbar dentro de header
+    </>
+  )
+}
+
+export default Navbar
