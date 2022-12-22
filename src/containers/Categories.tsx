266459deca47f5e0cba2bfaa9ecb141ce550607e
Christian Ruiz <christianjavieruizcortes@gmail.com>
2022-12-22 00:15:12 -0500

    categories almost done

--- src/containers/Categories.tsx
+++ src/containers/Categories.tsx
@@ -1,39 +1,44 @@
-import vegetarian from "../assets/icons/vegetarian.svg";
-import main from "../assets/icons/main.svg";
-// import cake from "../assets/icons/cake.svg";
-// import vegetarian from "../assets/icons/vegetarian.svg";
-// import vegetarian from "../assets/icons/vegetarian.svg";
-// import vegetarian from "../assets/icons/vegetarian.svg";
-// import vegetarian from "../assets/icons/vegetarian.svg";
-
+//icons
 const recipeIcons = require.context("../assets/icons", true);
 
 const Categories = () => {
   return (
     <ul className="c-categories">
       <li className="c-categories__item">
-        <a href="" className="c-categories__link">
-          <img src={vegetarian} alt="vegetarian" />
+        <a href="/" className="c-categories__link">
+          <img src={recipeIcons("./vegetarian.svg")} alt="vegetarian" />
           Vegetarian
         </a>
       </li>
       <li className="c-categories__item">
-        <a href="" className="c-categories__link">
-          <img src={main} alt="main" />
+        <a href="/" className="c-categories__link">
+          <img src={recipeIcons("./main.svg")} alt="principales" />
           Principales
         </a>
       </li>
       <li className="c-categories__item">
-        <a href="" className="c-categories__link"></a>
+        <a href="/" className="c-categories__link">
+          <img src={recipeIcons("./cake.svg")} alt="cake" />
+          Tortas
+        </a>
       </li>
       <li className="c-categories__item">
-        <a href="" className="c-categories__link"></a>
+        <a href="/" className="c-categories__link">
+          <img src={recipeIcons("./fast-food.svg")} alt="fast-food" />
+          Rápida
+        </a>
       </li>
       <li className="c-categories__item">
-        <a href="" className="c-categories__link"></a>
+        <a href="/" className="c-categories__link">
+          <img src={recipeIcons("./kids.svg")} alt="menu-kids" />
+          Menú niños
+        </a>
       </li>
       <li className="c-categories__item">
-        <a href="" className="c-categories__link"></a>
+        <a href="/" className="c-categories__link">
+          <img src={recipeIcons("./soup.svg")} alt="soup" />
+          Sopas
+        </a>
       </li>
     </ul>
   );
