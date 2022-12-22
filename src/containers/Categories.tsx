2325f11402f83ce758fe21e0b28c1879e9c49ace
Christian Ruiz <christianjavieruizcortes@gmail.com>
2022-12-22 00:59:47 -0500

    add colors

--- src/containers/Categories.tsx
+++ src/containers/Categories.tsx
@@ -5,38 +5,38 @@ const Categories = () => {
   return (
     <ul className="c-categories">
       <li className="c-categories__item">
+        <img src={recipeIcons("./vegetarian.svg")} alt="vegetarian" />
         <a href="/" className="c-categories__link">
-          <img src={recipeIcons("./vegetarian.svg")} alt="vegetarian" />
           Vegetarian
         </a>
       </li>
       <li className="c-categories__item">
+        <img src={recipeIcons("./main.svg")} alt="principales" />
         <a href="/" className="c-categories__link">
-          <img src={recipeIcons("./main.svg")} alt="principales" />
           Principales
         </a>
       </li>
       <li className="c-categories__item">
+        <img src={recipeIcons("./cake.svg")} alt="cake" />
         <a href="/" className="c-categories__link">
-          <img src={recipeIcons("./cake.svg")} alt="cake" />
           Tortas
         </a>
       </li>
       <li className="c-categories__item">
+        <img src={recipeIcons("./fast-food.svg")} alt="fast-food" />
         <a href="/" className="c-categories__link">
-          <img src={recipeIcons("./fast-food.svg")} alt="fast-food" />
           Rápida
         </a>
       </li>
       <li className="c-categories__item">
+        <img src={recipeIcons("./kids.svg")} alt="menu-kids" />
         <a href="/" className="c-categories__link">
-          <img src={recipeIcons("./kids.svg")} alt="menu-kids" />
           Menú niños
         </a>
       </li>
       <li className="c-categories__item">
+        <img src={recipeIcons("./soup.svg")} alt="soup" />
         <a href="/" className="c-categories__link">
-          <img src={recipeIcons("./soup.svg")} alt="soup" />
           Sopas
         </a>
       </li>
