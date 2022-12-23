fac6bf8e04f6f8e0c29fea17a73bd829030df96e
Christian Ruiz <christianjavieruizcortes@gmail.com>
2022-12-22 23:30:15 -0500

    mobile and tablet done for the recipe cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,68 @@
+// import Ojinjeo from "../assets/Ojingeo-muchim.png";
+// import Star from "../assets/icons/star.svg";
+// import Heart from "../assets/icons/favorite.svg";
+
+const recipe = require.context("../assets/", true);
+const icon = require.context("../assets/icons", true);
 
 const Card = () => {
   return (
-    <>
-      Card
-    </>
-  )
-}
+    <div className="c-card">
+      <div className="c-card__content-img">
+        <img
+          className="c-card__img"
+          src={recipe("./Ojingeo-muchim.png")}
+          alt="Food img"
+        />
+      </div>
+      <div className="c-card__ranking">
+        <p className="c-card__name">
+          Ojingeo <span>muchim</span>
+        </p>
+        <div className="c-card__footer">
+          <span>
+            <img className="c-card__icon" src={icon("./star.svg")} alt="star" />
+            5.0
+          </span>
+          <img
+            className="c-card__icon"
+            src={icon("./favorite.svg")}
+            alt="heart"
+          />
+        </div>
+      </div>
 
-export default Card
+      <div className="c-card__recipe">
+        <div className="c-card__info">
+          <img
+            className="c-card__recipe-icon"
+            src={icon("./portion.svg")}
+            alt="food portion"
+          />
+          <p className="c-card__recipe-title">Tamaño de la porcion</p>
+          <p className="c-card__recipe-data">4 raciones</p>
+        </div>
+        <div className="c-card__info">
+          <img
+            className="c-card__recipe-icon"
+            src={icon("./time.svg")}
+            alt="food time"
+          />
+          <p className="c-card__recipe-title">Tiempo de preparación</p>
+          <p className="c-card__recipe-data">10 minutos</p>
+        </div>
+        <div className="c-card__info">
+          <img
+            className="c-card__recipe-icon"
+            src={icon("./chef.svg")}
+            alt="chef hat"
+          />
+          <p className="c-card__recipe-title">Dificultad</p>
+          <p className="c-card__recipe-data">fácil</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
